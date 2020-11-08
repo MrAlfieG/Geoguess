57bd21426e83bdcb535780c118298c255f4c0fa2
MrAlfieG <74139070+MrAlfieG@users.noreply.github.com>
2020-11-08 20:47:28 +0000

    Update CardRoomSize.vue

--- src/components/widgets/card/CardRoomSize.vue
+++ src/components/widgets/card/CardRoomSize.vue
@@ -66,7 +66,35 @@
           {
             text: '5',
             value: 5,
-          },          
+          },  
+          {
+            text: '6',
+            value: 6,
+          }, 
+          {
+            text: '7',
+            value: 7,
+          }, 
+          {
+            text: '8',
+            value: 8,
+          }, 
+          {
+            text: '9',
+            value: 9,
+          }, 
+          {
+            text: '10',
+            value: 10,
+          }, 
+          {
+            text: '11',
+            value: 11,
+          }, 
+          {
+            text: '12',
+            value: 12,
+          }, 
         ],        
       }
     },
