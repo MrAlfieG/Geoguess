18614f90d0b19fda48b5ab8a203ec98a8de5fdc6
MrAlfieG <74139070+MrAlfieG@users.noreply.github.com>
2020-11-09 20:22:44 +0000

    Update TranslationsEN.ts

--- src/lang/TranslationsEN.ts
+++ src/lang/TranslationsEN.ts
@@ -4,10 +4,10 @@ export default {
       record: 'Record',
       singlePlayer: 'Single Player',
       about: 'ABOUT',
-      aboutDescription: 'Geoguess Master is a free and lazy geoguess game with no ads. \
+      aboutDescription: 'Geoguess Motonovo is a free and lazy geoguess game with no ads. \
         Players compete how close the player can guess random locations in five rounds. \
         You can share the score with other people via social media like Facebook or Twitter. \
-        You can play multiplayer game with your friends up to five friends. \
+        You can play multiplayer game with your friends up to twelve friends. \
         The first player creates a room and decide the room size. Other players type the same room name as the first player created and the game will start. ',
       limitation: 'LIMITATION',
       limitationDescription: "Currently I set quotas per day so the cost to run this game can't get too high. \
